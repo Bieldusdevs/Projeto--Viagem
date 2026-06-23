0af9dcb7570bf714dcd07b3573c49f96fae67a4f
Gabriel Carvalho <sadaxzzz8@gmail.com>
2026-06-23 15:43:22 +0100

    Remove unused import of Slot component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Slot } from "@radix-ui/react-slot" // Note: I'll simulate the Slot or just use standard components for simplicity in this environment
 import { cn } from "@/lib/utils"
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
